1e13aa6b84c93044ca55b280479f42b4206a1add
pipicaca123 <u9593103@gmail.com>
2019-03-02 22:35:07 +0800

    shiba

--- main.c
+++ main.c
@@ -5,19 +5,23 @@ int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
+int (*command[])(int,int)={plus,minus,multiply,divided};
+
 int main()
 {
-	int a, c;
+	int a, c,ch;
 	char b;
 	printf("key a Function \nEX: 1 + 1\n");
 	scanf("%i %c %i", &a, &b, &c);
 	//IF ELSE IF
 	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
+		ch=0;
 	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
+		ch=1;
 	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
+		ch=2;
 	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+		ch=3;
+
+	printf("%d %c %d =%d\n",a,b,c,command[ch](a,c));
 }
